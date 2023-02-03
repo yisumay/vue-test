8456873c8cad301f0aad689b7a55c92258cb2354
xielongmei <15577506835@163.com>
2023-02-03 18:08:03 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { effect, ref } from '@vue/reactivity';
+import HelloWorld from './components/HelloWorld.vue';
+import { renderer } from './services/test8-8';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  setup() {
+    const bol = ref(false);
+
+    effect(() => {
+      // 创建 vnode
+      const vnode = {
+        type: 'div',
+        props: bol.value
+          ? {
+              onClick: () => {
+                alert('父元素 clicked');
+              },
+            }
+          : {},
+        children: [
+          {
+            type: 'p',
+            props: {
+              onClick: () => {
+                bol.value = true;
+              },
+            },
+            children: 'text',
+          },
+        ],
+      };
+      // 渲染 vnode
+      renderer.render(vnode, document.querySelector('#app'));
+    });
+  },
+};
 </script>
 
 <style>
--- src/services/test8-8.js
+++ src/services/test8-8.js
@@ -1,29 +1,54 @@
-import { effect, ref } from '@vue/reactivity';
-
-const bol = ref(false);
-
 function createRenderer(options) {
   // 通过 options 得到操作 DOM 的 API
-  const { createElement, insert, setElementText } = options;
+  const { createElement, insert, setElementText, patchProps } = options;
 
   // 在这个作用域内定义的函数都可以访问那些 API
   function mountElement(vnode, container) {
-    // 调用 createElement 函数创建元素
-    const el = createElement(vnode.type);
+    // 让 vnode.el 引用真实 DOM 元素
+    const el = (vnode.el = createElement(vnode.type));
     if (typeof vnode.children === 'string') {
-      // 调用 setElementText 设置元素的文本节点
       setElementText(el, vnode.children);
+    } else if (Array.isArray(vnode.children)) {
+      vnode.children.forEach((child) => {
+        patch(null, child, el);
+      });
     }
-    // 调用 insert 函数将元素插入到容器内
+
+    if (vnode.props) {
+      for (const key in vnode.props) {
+        patchProps(el, key, null, vnode.props[key]);
+      }
+    }
+
     insert(el, container);
   }
 
+  function unmount(vnode) {
+    const parent = vnode.el.parentNode;
+    if (parent) {
+      parent.removeChild(vnode.el);
+    }
+  }
+
   function patch(n1, n2, container) {
-    // 如果 n1 不存在，意味着挂载，则调用 mountElement 函数完成挂载
-    if (!n1) {
-      mountElement(n2, container);
-    } else {
-      // n1 存在，意味着打补丁，暂时省略
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1);
+      n1 = null;
+    }
+    // 代码运行到这里，证明 n1 和 n2 所描述的内容相同
+    const { type } = n2;
+    // 如果 n2.type 的值是字符串类型，则它描述的是普通标签元素
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container);
+      } else {
+        // 打补丁
+        //  patchElement(n1, n2)
+      }
+    } else if (typeof type === 'object') {
+      // 如果 n2.type 的值的类型是对象，则它描述的是组件
+    } else if (type === 'xxx') {
+      // 处理其他类型的 vnode
     }
   }
 
@@ -43,15 +68,25 @@ function createRenderer(options) {
   };
 }
 
-const renderer = createRenderer({
+function shouldSetAsProps(el, key) {
+  // 特殊处理
+  if (key === 'form' && el.tagName === 'INPUT') return false;
+  // 兜底
+  return key in el;
+}
+
+export const renderer = createRenderer({
+  // 省略其他选项
   createElement(tag) {
-    // 省略部分代码
+    return document.createElement(tag);
   },
+  // 用于设置元素的文本节点
   setElementText(el, text) {
-    // 省略部分代码
+    el.textContent = text;
   },
+  // 用于在给定的 parent 下添加指定元素
   insert(el, parent, anchor = null) {
-    // 省略部分代码
+    parent.insertBefore(el, anchor);
   },
   createText(text) {
     return document.createTextNode(text);
@@ -59,17 +94,35 @@ const renderer = createRenderer({
   setText(el, text) {
     el.nodeValue = text;
   },
-  patchProps(el, key, prevValue, nextValue) {
-    // 省略部分代码
-  },
-});
-
-const renderer = createRenderer({
-  // 省略其他选项
 
   patchProps(el, key, prevValue, nextValue) {
-    // 对 class 进行特殊处理
-    if (key === 'class') {
+    if (/^on/.test(key)) {
+      const invokers = el._vei || (el._vei = {});
+      let invoker = invokers[key];
+      const name = key.slice(2).toLowerCase();
+      if (nextValue) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            // e.timeStamp 是事件发生的时间
+            // 如果事件发生的时间早于事件处理函数绑定的时间，则不执行事件处理函数
+            if (e.timeStamp < invoker.attached) return;
+            if (Array.isArray(invoker.value)) {
+              invoker.value.forEach((fn) => fn(e));
+            } else {
+              invoker.value(e);
+            }
+          };
+          invoker.value = nextValue;
+          // 添加 invoker.attached 属性，存储事件处理函数被绑定的时间
+          invoker.attached = performance.now();
+          el.addEventListener(name, invoker);
+        } else {
+          invoker.value = nextValue;
+        }
+      } else if (invoker) {
+        el.removeEventListener(name, invoker);
+      }
+    } else if (key === 'class') {
       el.className = nextValue || '';
     } else if (shouldSetAsProps(el, key, nextValue)) {
       const type = typeof el[key];
@@ -82,31 +135,4 @@ const renderer = createRenderer({
       el.setAttribute(key, nextValue);
     }
   },
-});
-
-effect(() => {
-  // 创建 vnode
-  const vnode = {
-    type: 'div',
-    props: bol.value
-      ? {
-          onClick: () => {
-            alert('父元素 clicked');
-          },
-        }
-      : {},
-    children: [
-      {
-        type: 'p',
-        props: {
-          onClick: () => {
-            bol.value = true;
-          },
-        },
-        children: 'text',
-      },
-    ],
-  };
-  // 渲染 vnode
-  renderer.render(vnode, document.querySelector('#app'));
 });
